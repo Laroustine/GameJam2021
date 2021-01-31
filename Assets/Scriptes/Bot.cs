97ae6d7fdd1f687614a9565e4abcd1779cbab144
Marzat <marzat@localhost.localdomain>
2021-01-31 02:04:03 +0100

    [ADD] A*
    it's the unity Pathfinding

---
+++ Assets/Scriptes/Bot.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bot : MonoBehaviour
+{
+    public int test;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider hit) {
+        
+    }
+}
